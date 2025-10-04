9072e9653f8f9f526e7d6f7a1e1a56b2088ddd0f
nabilmudz <nabilmudzaki306@gmail.com>
2025-10-04 16:03:42 +0700

    Fix: Routes By Nabil Mudzaki

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,120 +1,81 @@
-<nav x-data="{ open: false }" class="bg-white  border-b border-gray-100 ">
+<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <div class="shrink-0 flex items-center">
-                    <p class="text-2xl text-black font-bold mr-10   ">ETS Proyek 3</p>
+                    <p class="text-2xl font-bold text-black mr-10">ETS Proyek 3</p>
                 </div>
 
-                @if(Auth::user()->role === 'Admin')
-                <div class=" gap-8 mt-4">
-                    
-                    <x-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.users')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('admin.anggota.index')" :active="request()->routeIs('anggota.*')">
-                        {{ __('Anggota') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('admin.komponen_gaji.index')" :active="request()->routeIs('komponen_gaji.*')">
-                        {{ __('Komponen Gaji') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('admin.penggajian.index')" :active="request()->routeIs('penggajian.*')">
-                        {{ __('Penggajian') }}
-                    </x-nav-link>
+                <div class="hidden sm:flex gap-8 mt-4">
+                    @if(Auth::user()->role === 'Admin')
+                        <x-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">Dashboard</x-nav-link>
+                        <x-nav-link :href="route('admin.anggota.index')" :active="request()->routeIs('admin.anggota.*')">Anggota</x-nav-link>
+                        <x-nav-link :href="route('admin.komponen_gaji.index')" :active="request()->routeIs('admin.komponen_gaji.*')">Komponen Gaji</x-nav-link>
+                        <x-nav-link :href="route('admin.penggajian.index')" :active="request()->routeIs('admin.penggajian.*')">Penggajian</x-nav-link>
+                    @elseif(Auth::user()->role === 'Public')
+                        <x-nav-link :href="route('user.dashboard')" :active="request()->routeIs('user.dashboard')">Dashboard</x-nav-link>
+                        <x-nav-link :href="route('user.anggota.index')" :active="request()->routeIs('user.anggota.*')">Anggota</x-nav-link>
+                        <x-nav-link :href="route('user.penggajian.index')" :active="request()->routeIs('user.penggajian.*')">Penggajian</x-nav-link>
+                    @endif
                 </div>
-                @endif
-
-                @if(Auth::user()->role === 'Public')
-                    <div class=" gap-8 mt-4">
-                        <x-nav-link :href="route('user.dashboard')" :active="request()->routeIs('user.dashboard')">
-                            {{ __('Dashboard') }}
-                        </x-nav-link>
-                        <x-nav-link :href="route('admin.anggota.index')" :active="request()->routeIs('anggota.*')">
-                            {{ __('Anggota') }}
-                        </x-nav-link>
-                        <x-nav-link :href="route('admin.penggajian.index')" :active="request()->routeIs('penggajian.*')">
-                            {{ __('Penggajian') }}
-                        </x-nav-link>
-                    </div>
-                @endif
             </div>
 
-            <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition">
                             <div>{{ Auth::user()->username }}</div>
-
                             <div class="ml-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                <svg class="fill-current h-4 w-4" viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"/>
                                 </svg>
                             </div>
                         </button>
                     </x-slot>
 
                     <x-slot name="content">
-                        <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
-
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </x-dropdown-link>
+                            <x-dropdown-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">Log Out</x-dropdown-link>
                         </form>
                     </x-slot>
                 </x-dropdown>
             </div>
 
-            <!-- Hamburger -->
             <div class="-mr-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none transition">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        <path :class="{'hidden': open, 'inline-flex': ! open}" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"/>
+                        <path :class="{'hidden': ! open, 'inline-flex': open}" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
                     </svg>
                 </button>
             </div>
         </div>
     </div>
 
-    <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">\
-            @if(Auth::user()->role === 'admin')
-                <x-responsive-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
-                    {{ __('Dashboard') }}
-                </x-responsive-nav-link>
-            @endif
-
-            @if(Auth::user()->role === 'user')
-                <x-responsive-nav-link :href="route('user.dashboard')" :active="request()->routeIs('user.dashboard')">
-                    {{ __('Dashboard') }}
-                </x-responsive-nav-link>
+        <div class="pt-2 pb-3 space-y-1">
+            @if(Auth::user()->role === 'Admin')
+                <x-responsive-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">Dashboard</x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('admin.anggota.index')" :active="request()->routeIs('admin.anggota.*')">Anggota</x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('admin.komponen_gaji.index')" :active="request()->routeIs('admin.komponen_gaji.*')">Komponen Gaji</x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('admin.penggajian.index')" :active="request()->routeIs('admin.penggajian.*')">Penggajian</x-responsive-nav-link>
+            @elseif(Auth::user()->role === 'Public')
+                <x-responsive-nav-link :href="route('user.dashboard')" :active="request()->routeIs('user.dashboard')">Dashboard</x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('user.anggota.index')" :active="request()->routeIs('user.anggota.*')">Anggota</x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('user.penggajian.index')" :active="request()->routeIs('user.penggajian.*')">Penggajian</x-responsive-nav-link>
             @endif
         </div>
 
-        <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
-                <div class="font-medium text-base text-gray-800 ">{{ Auth::user()->name }}</div>
+                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
                 <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
             </div>
-
             <div class="mt-3 space-y-1">
-
-                <!-- Authentication -->
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
-
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
+                    <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">Log Out</x-responsive-nav-link>
                 </form>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -17,74 +17,72 @@ use App\Http\Controllers\PenggajianController;
 |
 */
 
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('auth.login');
+
 Route::get('/', function () {
-    $user = Auth::user();
+    $user = auth()->user();
+
     if (!$user) {
         return redirect()->route('auth.login');
     }
+
     if ($user->role === 'Admin') {
         return redirect()->route('admin.dashboard');
     }
 
     return redirect()->route('user.dashboard');
-});
-
-Route::prefix('/auth')->group(function () {
-    Route::get('/login', function () {
-        return view('auth.login');
-    })->name('auth.login');
-});
-
-Route::prefix('/dashboard')->group(function () {
-    Route::prefix('/admin')
-        ->middleware(['auth', 'role:Admin'])
-        ->group(function () {
-            Route::get('/', function () {
-                return view('admin.dashboard');
-            })->name('admin.dashboard');
-            
-            Route::prefix('/anggota')->group(function () {
-                Route::get('/', [AnggotaController::class, 'index'])->name('admin.anggota.index');
-                Route::get('/create', [AnggotaController::class, 'create'])->name('admin.anggota.create');
-                Route::post('/', [AnggotaController::class, 'store'])->name('admin.anggota.store');
-                Route::get('/{anggota}', [AnggotaController::class, 'show'])->name('admin.anggota.show');
-                Route::get('/{anggota}/edit', [AnggotaController::class, 'edit'])->name('admin.anggota.edit');
-                Route::put('/{anggota}', [AnggotaController::class, 'update'])->name('admin.anggota.update');
-                Route::delete('/{anggota}', [AnggotaController::class, 'destroy'])->name('admin.anggota.destroy'); 
-            });
-
-            Route::prefix('/komponen-gaji')->group(function () {
-                Route::get('/', [KomponenGajiController::class, 'index'])->name('admin.komponen_gaji.index');
-                Route::get('/create', [KomponenGajiController::class, 'create'])->name('admin.komponen_gaji.create');
-                Route::post('/', [KomponenGajiController::class, 'store'])->name('admin.komponen_gaji.store');
-                Route::get('/{komponenGaji}', [KomponenGajiController::class, 'show'])->name('admin.komponen_gaji.show');
-                Route::get('/{komponenGaji}/edit', [KomponenGajiController::class, 'edit'])->name('admin.komponen_gaji.edit');
-                Route::put('/{komponenGaji}', [KomponenGajiController::class, 'update'])->name('admin.komponen_gaji.update');
-                Route::delete('/{komponenGaji}', [KomponenGajiController::class, 'destroy'])->name('admin.komponen_gaji.destroy'); 
-                Route::get('/by-jabatan/{jabatan}', [KomponenGajiController::class, 'showByJabatan'])->name('admin.komponen_gaji.showByJabatan');
-            });
-
-            Route::prefix('/penggajian')->group(function () {
-                Route::get('/', [PenggajianController::class, 'index'])->name('admin.penggajian.index');
-                Route::get('/create', [PenggajianController::class, 'create'])->name('admin.penggajian.create');
-                Route::post('/', [PenggajianController::class, 'store'])->name('admin.penggajian.store');
-                Route::get('/{id_komponen_gaji}/{id_anggota}', [PenggajianController::class, 'show'])->name('admin.penggajian.show');
-                Route::get('/{id_komponen_gaji}/{id_anggota}/edit', [PenggajianController::class, 'edit'])->name('admin.penggajian.edit');
-                Route::put('/{id_komponen_gaji}/{id_anggota}', [PenggajianController::class, 'update'])->name('admin.penggajian.update');
-                Route::delete('/{id_komponen_gaji}/{id_anggota}', [PenggajianController::class, 'destroy'])->name('admin.penggajian.destroy'); 
-            });
-        });
-
-    Route::prefix('/public')
-        ->middleware(['auth', 'role:Public'])
-        ->group(function () {
-        Route::get('/', function () {
-            return view('user.dashboard');
-        })->name('user.dashboard');
-        Route::get('/anggota-dpr', [AnggotaController::class, 'index'])->name('admin.anggota.index');
-        Route::get('/gaji-dpr', [PenggajianController::class, 'index'])->name('admin.penggajian.index');
-    });
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::prefix('/admin')
+    ->middleware(['auth', 'role:Admin'])
+    ->group(function () {
+    Route::get('/', function () {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
+    
+    Route::prefix('/anggota')->group(function () {
+        Route::get('/', [AnggotaController::class, 'index'])->name('admin.anggota.index');
+        Route::get('/create', [AnggotaController::class, 'create'])->name('admin.anggota.create');
+        Route::post('/', [AnggotaController::class, 'store'])->name('admin.anggota.store');
+        Route::get('/{anggota}', [AnggotaController::class, 'show'])->name('admin.anggota.show');
+        Route::get('/{anggota}/edit', [AnggotaController::class, 'edit'])->name('admin.anggota.edit');
+        Route::put('/{anggota}', [AnggotaController::class, 'update'])->name('admin.anggota.update');
+        Route::delete('/{anggota}', [AnggotaController::class, 'destroy'])->name('admin.anggota.destroy'); 
+    });
+
+    Route::prefix('/komponen-gaji')->group(function () {
+        Route::get('/', [KomponenGajiController::class, 'index'])->name('admin.komponen_gaji.index');
+        Route::get('/create', [KomponenGajiController::class, 'create'])->name('admin.komponen_gaji.create');
+        Route::post('/', [KomponenGajiController::class, 'store'])->name('admin.komponen_gaji.store');
+        Route::get('/{komponenGaji}', [KomponenGajiController::class, 'show'])->name('admin.komponen_gaji.show');
+        Route::get('/{komponenGaji}/edit', [KomponenGajiController::class, 'edit'])->name('admin.komponen_gaji.edit');
+        Route::put('/{komponenGaji}', [KomponenGajiController::class, 'update'])->name('admin.komponen_gaji.update');
+        Route::delete('/{komponenGaji}', [KomponenGajiController::class, 'destroy'])->name('admin.komponen_gaji.destroy'); 
+        Route::get('/by-jabatan/{jabatan}', [KomponenGajiController::class, 'showByJabatan'])->name('admin.komponen_gaji.showByJabatan');
+    });
+
+    Route::prefix('/penggajian')->group(function () {
+        Route::get('/', [PenggajianController::class, 'index'])->name('admin.penggajian.index');
+        Route::get('/create', [PenggajianController::class, 'create'])->name('admin.penggajian.create');
+        Route::post('/', [PenggajianController::class, 'store'])->name('admin.penggajian.store');
+        Route::get('/{id_komponen_gaji}/{id_anggota}', [PenggajianController::class, 'show'])->name('admin.penggajian.show');
+        Route::get('/{id_komponen_gaji}/{id_anggota}/edit', [PenggajianController::class, 'edit'])->name('admin.penggajian.edit');
+        Route::put('/{id_komponen_gaji}/{id_anggota}', [PenggajianController::class, 'update'])->name('admin.penggajian.update');
+        Route::delete('/{id_komponen_gaji}/{id_anggota}', [PenggajianController::class, 'destroy'])->name('admin.penggajian.destroy'); 
+    });
+});
+
+Route::prefix('/public')
+    ->middleware(['auth', 'role:Public'])
+    ->group(function () {
+    Route::get('/', function () {
+        return view('user.dashboard');
+    })->name('user.dashboard');
+    Route::get('/anggota-dpr', [AnggotaController::class, 'index'])->name('user.anggota.index');
+    Route::get('/gaji-dpr', [PenggajianController::class, 'index'])->name('user.penggajian.index');
+});
 
 Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
